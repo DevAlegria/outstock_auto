5a8fc28463add826a8df78371a6b2d889daad97f
DevAlegria <yhonier.alegria@gmail.com>
2026-01-28 09:52:57 -0500

    feat : list product

--- main.py
+++ main.py
@@ -13,8 +13,22 @@ INNER JOIN tblDetallePOS d
 WHERE p.dtFecha_Compra >= DATEADD(HOUR, -1, GETDATE())
 ORDER BY p.dtFecha_Compra DESC'''
 
-cursor = get_db_connection().cursor()
+getProduct = """SELECT
+	strReferencia,
+	strCodigo,
+	strDescripcion,
+	intCantidadMinimaAviso,
+	intCantidad,
+	strIdProveedor
+FROM tblInventario where strReferencia = '9903907014068'"""
+
+connection = get_db_connection()
+cursor = connection.cursor()
 cursor.execute(getOutStock)
 
 for row in cursor:
+    reference = row.strReferencia
+    product = connection.cursor()
+    product.execute(getProduct).fetchone()
+    print(product)
     print(row)
