fc97a7fea4231cbca38fc27417e101f9cdf43f99
DevAlegria <yhonier.alegria@gmail.com>
2026-02-02 08:19:30 -0500

    feat: implement ReportWriter class for generating Markdown reports

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from src.database.db_config import getDbConnection
 from src.database.sql_query import SifacPOS
+from src.writer import ReportWriter
 
 def main():
     conn = getDbConnection()
     cursor = conn.cursor()
     sifacPOS = SifacPOS(cursor)
+    reporter = ReportWriter('sales_report', ['Reference','Description'])
 
     latestSales = sifacPOS.getLatestSales()
-    print("Latest Sales:")
+
     for sale in latestSales:
-        print(sale)
+        reference = sale.strReferencia
+        productDetails = sifacPOS.getProductDetails(reference)
+
+        if productDetails:
+            row = [
+                productDetails.strReferencia,
+                productDetails.strDescripcion
+            ]
+            reporter.append_row(row)
 
     cursor.close()
     conn.close()
